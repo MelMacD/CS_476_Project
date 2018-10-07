74354201ed636d669b9c32373556b3e954281614
MelMacD <37010895+MelMacD@users.noreply.github.com>
2018-10-06 19:21:19 -0600

    Update login.py

--- code/login.py
+++ code/login.py
@@ -1,8 +1,14 @@
 from code import app
 
+class Login:
+    def init(self):
+        html = "Log In"
+        return html
 
 @app.route("/login")
 
 def login():
+    
+    loginHTML = Login()
 
-    return "Log In"
+    return loginHTML
