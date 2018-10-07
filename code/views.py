2db6fb88effe410a81a74c71b5ef92ee86b87e63
MelMacD <37010895+MelMacD@users.noreply.github.com>
2018-10-06 19:01:00 -0600

    Update views.py
    Reduce view.py

--- code/views.py
+++ code/views.py
@@ -6,13 +6,3 @@ from code import app
 def hello():
 
     return "Hello World!"
-
-@app.route("/login")
-
-def login():
-    return "Login Page"
-
-@app.route("/signup")
-
-def signup():
-    return "Sign Up"
