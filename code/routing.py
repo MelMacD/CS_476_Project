6dad8ab8081657337d237bb1be32e32d2dd3eded
MelMacD <37010895+MelMacD@users.noreply.github.com>
2018-10-23 16:13:43 -0600

    Update routing.py

--- code/routing.py
+++ code/routing.py
@@ -6,13 +6,11 @@ from code import app
 def hello():
 
     return """
-<div class="panel panel-default">
-  <div class="panel-body">
+<div class="border border-dark rounded">
     <h1>
         What is the title?
     </h1>
     <p>
         What is the content?
     </p>
-  </div>  
 </div>"""
