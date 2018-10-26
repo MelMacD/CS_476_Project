7579b0984cf76f23595a1d4418761f3e2fb64be6
MelMacD <37010895+MelMacD@users.noreply.github.com>
2018-10-25 20:23:21 -0600

    Update blogEditingHandlers.js

--- code/static/blogEditingHandlers.js
+++ code/static/blogEditingHandlers.js
@@ -1,7 +1,17 @@
 $(document).ready(function() {
-    $("#toggleEditing").click(function () {
-        $("#addText").css("display", "block");
-        $("#addImage").css("display", "block");
-        $("#addVideo").css("display", "block");
+    $("#enableEditing").click(function () {
+        $("#enableEditing").css("display", "none");
+        $("#disableEditing").css("display", "inline");
+        $("#addText").css("display", "inline");
+        $("#addImage").css("display", "inline");
+        $("#addVideo").css("display", "inline");
+    });
+    
+    $("#disableEditing").click(function() {
+        $("#enableEditing").css("display", "inline");
+        $("#disableEditing").css("display", "none");
+        $("#addText").css("display", "none");
+        $("#addImage").css("display", "none");
+        $("#addVideo").css("display", "none");
     });
 });
