dac7a0165f6a4d78121fb3449a7e3471691b32ac
MelMacD <37010895+MelMacD@users.noreply.github.com>
2018-10-29 19:57:20 -0600

    Update signup.py
    change tabs to spaces

--- code/signup.py
+++ code/signup.py
@@ -62,17 +62,17 @@ class SignUp:
 
 def signup():
     if request.method == 'POST':
-	server = 'expressyourself.database.windows.net'
-	database = 'expressyourself'
-	username = 'cs476'
-	password = '$up3rSecret'
-	driver= '{ODBC Driver 13 for SQL Server}'
-	cnxn = pyodbc.connect('DRIVER='+driver+';SERVER='+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password)
-	cursor = cnxn.cursor()
-	cursor.execute("INSERT INTO users VALUES ('${username}', '${email}', '${pwd}', 0, null)".format(
-		username=request.form.get("username"),
-		password=request.form.get("pwd"),
-		email=request.form.get("email")));
+        server = 'expressyourself.database.windows.net'
+        database = 'expressyourself'
+        username = 'cs476'
+        password = '$up3rSecret'
+        driver= '{ODBC Driver 13 for SQL Server}'
+        cnxn = pyodbc.connect('DRIVER='+driver+';SERVER='+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password)
+        cursor = cnxn.cursor()
+        cursor.execute("INSERT INTO users VALUES ('${username}', '${email}', '${pwd}', 0, null)".format(
+                username=request.form.get("username"),
+                password=request.form.get("pwd"),
+                email=request.form.get("email")));
         return "Sign up request received"
     else:
         signupHTML = SignUp()
