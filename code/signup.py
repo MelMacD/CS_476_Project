f3ad0034a7e09fba98fcde6e4b73c72a07f80c28
MelMacD <37010895+MelMacD@users.noreply.github.com>
2018-10-30 12:25:29 -0600

    Update signup.py

--- code/signup.py
+++ code/signup.py
@@ -67,7 +67,7 @@ def signup():
         username = 'cs476'
         password = '$up3rSecret'
         driver= '{ODBC Driver 13 for SQL Server}'
-        #cnxn = pyodbc.connect('DRIVER='+driver+';SERVER='+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password)
+        cnxn = pyodbc.connect('DRIVER='+driver+';SERVER='+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password)
         #cursor = cnxn.cursor()
         #cursor.execute("INSERT INTO users VALUES ('${username}', '${email}', '${pwd}', 0, null)".format(
         #        username=request.form.get("username"),
