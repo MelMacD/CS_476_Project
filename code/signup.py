3bbc4e294fc5c3a3927cdb8864403d579c2bc825
MelMacD <37010895+MelMacD@users.noreply.github.com>
2018-10-30 12:26:46 -0600

    Update signup.py

--- code/signup.py
+++ code/signup.py
@@ -69,10 +69,10 @@ def signup():
         driver= '{ODBC Driver 13 for SQL Server}'
         cnxn = pyodbc.connect('DRIVER='+driver+';SERVER='+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password)
         cursor = cnxn.cursor()
-        #cursor.execute("INSERT INTO users VALUES ('${username}', '${email}', '${pwd}', 0, null)".format(
-        #        username=request.form.get("username"),
-        #        password=request.form.get("pwd"),
-        #        email=request.form.get("email")));
+        cursor.execute("INSERT INTO users VALUES ('${username}', '${email}', '${pwd}', 0, null)".format(
+                username=request.form.get("username"),
+                password=request.form.get("pwd"),
+                email=request.form.get("email")));
         return "Sign up request received"
     else:
         signupHTML = SignUp()
