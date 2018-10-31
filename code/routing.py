e11a651ee36b8d40d14e2a383e1037fa149e2f47
MelMacD <37010895+MelMacD@users.noreply.github.com>
2018-10-30 22:10:08 -0600

    Update routing.py

--- code/routing.py
+++ code/routing.py
@@ -13,7 +13,8 @@ def hello():
         file = request.files['testFile']
         if file:
             filename = secure_filename(file.filename)
-            path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            #path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            path = "uploads/images/" + filename
             file.save(path)
             return str(path)
     else:
