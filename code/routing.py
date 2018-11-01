3b67d69606b499edf76239b8d8f01826ca10df11
MelMacD <37010895+MelMacD@users.noreply.github.com>
2018-10-31 20:32:31 -0600

    Update routing.py

--- code/routing.py
+++ code/routing.py
@@ -46,7 +46,7 @@ def hello():
     </div>
     
 <!-- Modal -->
-    <div class="modal" id="exampleModal" tabindex="-1" role="dialog">
+    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog modal-dialog-centered" role="document">
     <div class="modal-content">
       <div class="modal-header">
