ee94362a51664ece463d038c7d96951228405674
MelMacD <37010895+MelMacD@users.noreply.github.com>
2018-10-31 21:03:06 -0600

    Update routing.py

--- code/routing.py
+++ code/routing.py
@@ -57,24 +57,7 @@ def hello():
         </button>
       </div>
       <div class="modal-body">
-        <form>
-          <div class="form-group">
-            <label for="recipient-name" class="col-form-label">Header:</label>
-            <input type="text" class="form-control" id="postTitle">
-          </div>
-          <div class="form-group">
-            <label for="message-text" class="col-form-label">Content:</label>
-            <textarea class="form-control" id="postContent"></textarea>
-          </div>
-          <div class="form-group">
-            <label for="message-text" class="col-form-label">Font Colour:</label>
-            <input type="color" class="form-control" id="postFontColor" value="#000000">
-          </div>
-          <div class="form-group">
-            <label for="message-text" class="col-form-label">Background Colour:</label>
-            <input type="color" class="form-control" id="postBackgroundColor" value="#000000">
-          </div>
-        </form>
+        Error
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
