70e9f7fcac180a38bce052aad729e4a3081d7f99
MelMacD <37010895+MelMacD@users.noreply.github.com>
2018-10-31 20:38:15 -0600

    Update routing.py

--- code/routing.py
+++ code/routing.py
@@ -51,17 +51,26 @@ def hello():
   <div class="modal-dialog modal-dialog-centered" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <h5 class="modal-title" id="exampleModalLabel">Edit Content</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
       <div class="modal-body">
-        Body content
+        <form>
+          <div class="form-group">
+            <label for="recipient-name" class="col-form-label">Header:</label>
+            <input type="text" class="form-control" id="postTitle">
+          </div>
+          <div class="form-group">
+            <label for="message-text" class="col-form-label">Content:</label>
+            <textarea class="form-control" id="postContent"></textarea>
+          </div>
+        </form>
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
+        <button id="saveChanges" type="button" class="btn btn-primary">Save changes</button>
       </div>
     </div>
   </div>
