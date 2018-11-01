da638746a67cb7343e0cdb3e9c203094ec2a26d2
MelMacD <37010895+MelMacD@users.noreply.github.com>
2018-11-01 12:34:33 -0600

    Update blogEditingHandlers.js

--- code/static/blogEditingHandlers.js
+++ code/static/blogEditingHandlers.js
@@ -1,6 +1,8 @@
-var postHtml = `<div class="border border-dark rounded draggable resizable" style="background-color: grey; opacity: 0.4; width: 350px; height: 400px;">
-               <button type="button" data-toggle="modal" data-target="#exampleModal">Edit Post</button>
+var postHtml = `<div id="mask" class="border border-dark rounded draggable resizable" style="background-color: grey; opacity: 0.4; width: 350px; height: 400px;">
+                 <div id="originalContent">
+                   <button type="button" data-toggle="modal" data-target="#exampleModal">Edit Post</button>
                    <h3>What is the title?</h3><p>What is the content?</p>
+                 </div>
                </div>`;
 var imageHtml = `<div class="draggable">
                 <img class="resizable" src="static/very_important.jpg" width="500" height="500">
