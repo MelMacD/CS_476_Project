129251ad72c7cc6197fcca298855cde13bae9b28
MelMacD <37010895+MelMacD@users.noreply.github.com>
2018-11-05 00:01:46 -0600

    Update routing.py

--- code/routing.py
+++ code/routing.py
@@ -29,7 +29,7 @@ def getBlobVideos():
 
 def hello():
     if request.method == 'POST':
-        return str(request.form.get['upload'])
+        return str(request.form.get('upload'))
         try:
             file = request.files['testFile']
             if file:
