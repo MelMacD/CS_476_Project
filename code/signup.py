dc2de39b8d8ed9e8bf00ac4485f7697f4098819c
Iyanu01 <37347177+Iyanu01@users.noreply.github.com>
2018-11-06 20:17:11 -0700

    Update signup.py

--- code/signup.py
+++ code/signup.py
@@ -12,50 +12,134 @@ class SignUp:
     
     def setHTML(self):
         self.html = """
-<head>
-<title>Sign-up Page</title>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<link rel="stylesheet" href="static/bootstrap.css" />
-</head>
+<!DOCTYPE html>
+<html lang="en">
+<title>Sign-up Form</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<style>
+body {font-family: "Lato", sans-serif}
+.mySlides {display: none}
+    
+     {box-sizing: border-box
+}
+    /* Full-width input fields */
+  input[type=text], input[type=password] {
+  width: 100%;
+  padding: 15px;
+  margin: 5px 0 22px 0;
+  display: inline-block;
+  border: none;
+  background: #f1f1f1;
+}
+
+input[type=text]:focus, input[type=password]:focus {
+  background-color: #ddd;
+  outline: none;
+}
+
+hr {
+  border: 1px solid #f1f1f1;
+  margin-bottom: 25px;
+}
+
+/* Set a style for all buttons */
+button {
+  background-color: #4CAF50;
+  color: white;
+  padding: 14px 20px;
+  margin: 8px 0;
+  border: none;
+  cursor: pointer;
+  width: 100%;
+  opacity: 0.9;
+}
+
+button:hover {
+  opacity:1;
+}
+
+/* Extra styles for the cancel button */
+.cancelbtn {
+  padding: 14px 20px;
+  background-color: #f44336;
+}
+
+/* Float cancel and signup buttons and add an equal width */
+.cancelbtn, .signup {
+  float: left;
+  width: 50%;
+}
+
+/* Add padding to container elements */
+.container {
+  padding: 16px;
+}
+
+/* Clear floats */
+.clearfix::after {
+  content: "";
+  clear: both;
+  display: table;
+}
+
+/* Change styles for cancel button and signup button on extra small screens */
+@media screen and (max-width: 300px) {
+  .cancelbtn, .signup {
+    width: 100%;
+  }
+}
+    
+    
+</style>
+
 <body>
-<nav class="navbar navbar-light bg-light">
-  <div class="container-fluid">
-  <div class="navbar-header">
-  <a class="navbar-brand" href="/">Express Yourself</a>
-  </div>
-  <ul class="nav navbar-nav navbar-right">
-    <li><a href="/login">Login</a></li>
-    <li><a href="/signup">Sign Up</a></li>
-  </ul>
-  </div>
-</nav>
+
 <form id="SignUp" style="width:500px" method="post" enctype="multipart/form-data">
-  <div class="container">
-    <label><b>Email</b></label>
-    <label id="email_msg" class="err_msg"></label>
-    <input id="email" type="text" placeholder="Enter Email" size="30" name="email">
-    </br>
+ 
     
-    <label><b>Username</b></label>
+     
+    
+    <div class="w3-container">
+ 
+            
+           <p>Please fill in this form to create an account.</p>
+      <hr>
+          <label><b>Username</b></label>
     <label id="username_msg" class="err_msg"></label>
-    <input id="username" type="text" placeholder="Enter Username" size="30" name="username">
-    </br>
-	
-    <label><b>Password</b></label>
+     <input class="w3-input w3-border" id="username" type="text" placeholder="Enter Username" size="30" name="username">
+          
+      <label><b>Email</b></label>
+    <label id="email_msg" class="err_msg"></label>
+      <input class="w3-input w3-border" id="email" type="text" placeholder="Enter Email" size="30" name="email" required>
+
+      <label><b>Password</b></label>
      <label id="pswd_msg" class="err_msg"></label>
-    <input  type="password" placeholder="Enter Password" size="30" name="pwd">
-    
-    <label><b>Re-enter Password</b></label>
+        
+      <input class="w3-input w3-border" type="password" placeholder="Enter Password" size="30" name="pwd" required>
+
+      <label><b>Re-enter Password</b></label>
      <label id="reEnter_msg" class="err_msg"></label>
-    <input  type="password" placeholder="Re-enter Password" size="30" name="pwd2">
-     <input type="submit" name="Signup" value="Signup"/>
-     <a class="dropdown-item" href="/login">Already have an account? Sign in</a>
-     </br>
-  </div>
+        
+      <input class="w3-input w3-border" type="password" placeholder="Re-enter Password" size="30"   name="pwd2" required>
+    
+     <div class="clearfix">
+      <button type="button" class="cancelbtn">Cancel</button>
+      <button type="submit"  class="signup" name="Signup" value="Signup">Sign-up</button>
+        </div>
+
+          <a class="dropdown-item" href="sample.html">Already have an account? Sign in</a>
+        <p class="w3-right">Need <a href="#" class="w3-text-blue">help?</a></p>
+      </div>
+  
 </form>
     
 </body>
 </html>
+
 """
 
 @app.route("/signup", methods=['GET', 'POST'])
