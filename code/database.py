4926d20ef673f31ea6d42d68c4186010d9f56ec6
MelMacD <37010895+MelMacD@users.noreply.github.com>
2018-11-06 23:29:05 -0600

    Update database.py
    implement database class

--- code/database.py
+++ code/database.py
@@ -1,2 +1,30 @@
+import pyodbc
 
 class Database:
+    def __init__(self, tableName):
+        self.table = tableName
+        self.server = 'tcp:expressyourself.database.windows.net'
+        self.database = 'expressyourself'
+        self.username = 'cs476'
+        self.password = '$up3rSecret'
+        self.driver = '{ODBC Driver 13 for SQL Server}'
+        self.connection = connect()
+      
+    def connect(self):
+        try:
+            return pyodbc.connect('DRIVER='+driver+';SERVER='+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password)
+        except Exception as e:
+            return e
+    
+    def execute(self, isModifying, command):
+        try:
+            cursor = self.connection.cursor()
+            cursor.execute(command)
+            if isModifying:
+                self.connection.commit()
+                return "Success"
+            else:
+                return cursor.fetchall()
+        except pyodbc.Error as e:
+            sqlstate = ex.args[1]
+            return sqlstate
