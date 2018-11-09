e1443a1b303e73f6be54d9187ef8b04e17fe2f43
MelMacD <37010895+MelMacD@users.noreply.github.com>
2018-11-09 11:52:21 -0600

    Create blobStorage.py
    make blob storage class

---
+++ blobStorage.py
@@ -0,0 +1,74 @@
+from werkzeug.utils import secure_filename
+from azure.storage.blob import BlockBlobService, PublicAccess
+
+class BlobStorage:
+    def __init__(self):
+        self.accountName = "expressiveblob"
+        self.accountKey = "F2G8lu/eZ6PduDIJFksWvuItZdhf+GONR2wgwgSsJMUO4s0mMdFI6PiC7K7ypcMSOH6m5kPhn2C9ketBRQiyKA=="
+
+    def makeService(self):
+        return BlockBlobService(account_name=self.accountName, account_key=self.accountKey)
+#could implement more in the class
+
+@app.route("/getBlobImages")
+
+def getBlobImages():
+    blobStorage = BlobStorage()
+    block_blob_service = blobStorage.makeService()
+    generator = block_blob_service.list_blobs('images')
+    blobList = []
+    for blob in generator:
+        blobList.append(blob.name)
+    return json.dumps(blobList)
+
+@app.route("/getBlobVideos")
+
+def getBlobVideos():
+    blobStorage = BlobStorage()
+    block_blob_service = blobStorage.makeService()
+    generator = block_blob_service.list_blobs('videos')
+    blobList = []
+    for blob in generator:
+        blobList.append(blob.name)
+    return json.dumps(blobList)
+
+@app.route("/uploadBlobImage", methods=['GET', 'POST'])
+
+def uploadBlobImage():
+    if request.method == 'POST':
+        try:
+            file = request.files['imageFile']
+            if file:
+                filename = secure_filename(file.filename)
+                blobStorage = BlobStorage()
+                block_blob_service = blobStorage.makeService()
+                container = 'images'
+                block_blob_service.create_blob_from_stream(container, filename, file)
+                return "Success"
+            else:
+                return "Invalid File"
+        except Exception as e:
+            return e
+    else:
+        return "Error"
+
+@app.route("/uploadBlobVideo", methods=['GET', 'POST'])
+
+def uploadBlobVideo():
+    if request.method == 'POST':
+        try:
+            file = request.files['videoFile']
+            if file:
+                filename = secure_filename(file.filename)
+                blobStorage = BlobStorage()
+                block_blob_service = blobStorage.makeService()
+                container = 'videos'
+                block_blob_service.create_blob_from_stream(container, filename, file)
+                return "Success"
+            else:
+                return "Invalid File"
+        except Exception as e:
+            return e
+    else:
+        return "Error"
+    
