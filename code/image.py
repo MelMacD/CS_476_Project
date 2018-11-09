0b09cae2bc458a5e6a684fc5da6e675ac5421021
MelMacD <37010895+MelMacD@users.noreply.github.com>
2018-11-09 11:26:11 -0600

    Create image.py
    make image class file

---
+++ code/image.py
@@ -0,0 +1,30 @@
+class Image:
+    def __init__(self, row):
+        self.html = self.buildHtml()
+        #setLocation(row.get("top"), row.get("left"))
+        #setSize(row.get("width"), row.get("height"))
+        #setDepth(row.get("depth"));
+
+    #@override
+    def buildHtml(self):
+        return """
+<div class="draggable resizableAspect" style="width: {width}px; height: {height}px; position: absolute; z-index: {depth}; left: {left}px; top: {top}px;">
+    <button class="editImage edit" type="button" style="position: absolute; top: 0; right: 0;" data-toggle="modal" data-target="#exampleModal">Edit</button>
+    <img src="{src}" style="width: 100%; height: 100%;">
+</div>"""
+      
+    #@override
+    def setLocation(self, top, left):
+         return self.html.format(top=top, left=left)
+     
+    #@override
+    def setSize(self, width, height):
+         return self.html.format(width=width, height=height)
+     
+    #@override
+    def setDepth(self, depth):
+        return self.html.format(depth=depth)
+      
+    def setImage(self, source):
+        return self.html.format(src=source)
+         
