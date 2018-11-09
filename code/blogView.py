b4da5e13bd63692f1c45bb190ee711a3cc0d13a0
MelMacD <37010895+MelMacD@users.noreply.github.com>
2018-11-09 11:54:51 -0600

    Update and rename routing.py to blogView.py
    move blob functions into separate file, rename

--- code/blogView.py
+++ code/blogView.py
@@ -4,64 +4,6 @@ import json
 from flask import request
 from werkzeug.utils import secure_filename
 from azure.storage.blob import BlockBlobService, PublicAccess
-
-@app.route("/getBlobImages")
-
-def getBlobImages():
-    block_blob_service = BlockBlobService(account_name='expressiveblob', account_key='F2G8lu/eZ6PduDIJFksWvuItZdhf+GONR2wgwgSsJMUO4s0mMdFI6PiC7K7ypcMSOH6m5kPhn2C9ketBRQiyKA==')
-    generator = block_blob_service.list_blobs('images')
-    blobList = []
-    for blob in generator:
-        blobList.append(blob.name)
-    return json.dumps(blobList)
-
-@app.route("/getBlobVideos")
-
-def getBlobVideos():
-    block_blob_service = BlockBlobService(account_name='expressiveblob', account_key='F2G8lu/eZ6PduDIJFksWvuItZdhf+GONR2wgwgSsJMUO4s0mMdFI6PiC7K7ypcMSOH6m5kPhn2C9ketBRQiyKA==')
-    generator = block_blob_service.list_blobs('videos')
-    blobList = []
-    for blob in generator:
-        blobList.append(blob.name)
-    return json.dumps(blobList)
-
-@app.route("/uploadBlobImage", methods=['GET', 'POST'])
-
-def uploadBlobImage():
-    if request.method == 'POST':
-        try:
-            file = request.files['imageFile']
-            if file:
-                filename = secure_filename(file.filename)
-                block_blob_service = BlockBlobService(account_name='expressiveblob', account_key='F2G8lu/eZ6PduDIJFksWvuItZdhf+GONR2wgwgSsJMUO4s0mMdFI6PiC7K7ypcMSOH6m5kPhn2C9ketBRQiyKA==')
-                container = 'images'
-                block_blob_service.create_blob_from_stream(container, filename, file)
-                return "Success"
-            else:
-                return "Invalid File"
-        except Exception as e:
-            return e
-    else:
-        return "Error"
-
-@app.route("/uploadBlobVideo", methods=['GET', 'POST'])
-
-def uploadBlobVideo():
-    if request.method == 'POST':
-        try:
-            file = request.files['videoFile']
-            if file:
-                filename = secure_filename(file.filename)
-                block_blob_service = BlockBlobService(account_name='expressiveblob', account_key='F2G8lu/eZ6PduDIJFksWvuItZdhf+GONR2wgwgSsJMUO4s0mMdFI6PiC7K7ypcMSOH6m5kPhn2C9ketBRQiyKA==')
-                container = 'videos'
-                block_blob_service.create_blob_from_stream(container, filename, file)
-                return "Success"
-            else:
-                return "Invalid File"
-        except Exception as e:
-            return e
-    else:
-        return "Error"
     
 @app.route("/blogView", methods=['GET', 'POST'])
 
