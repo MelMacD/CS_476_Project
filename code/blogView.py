ed0b86a7018943c26e21b540e5f784e9328419bf
MelMacD <37010895+MelMacD@users.noreply.github.com>
2018-11-10 13:49:29 -0600

    Update blogView.py

--- code/blogView.py
+++ code/blogView.py
@@ -110,7 +110,7 @@ def hello():
   <div class="container">
     <button type="button" id="enableEditing" class="btn btn-default">Enable Edit Mode</button>
     <button type="button" style="display:none;" id="disableEditing" class="btn btn-default">Exit Edit Mode</button>
-    <button type="button" style="display:none;" id="saveChanges" class="btn btn-success edit">Save Changes</button>
+    <button type="button" style="display:inline;" id="saveChanges" class="btn btn-default">Save Changes</button>
   </div>
 </footer
 </body>"""
