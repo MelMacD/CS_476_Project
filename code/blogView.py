2bf85b2a112cb3c5775b4dc4315aa777ce0ae9e3
MelMacD <37010895+MelMacD@users.noreply.github.com>
2018-11-10 14:55:04 -0600

    Update blogView.py

--- code/blogView.py
+++ code/blogView.py
@@ -7,7 +7,7 @@ import json
 def hello():
     if request.method == 'POST':
         jsonRequest = request.get_json(silent=True)
-        jsonDict = json.loads(jsonRequest)
+        jsonDict = json.loads('{"id":"XXXX", "name": "xyz", "user" : { "id": "XXXX", "username":"XYZ", "group":{"id": "XXXX"}}}')
         return str("Hi")
     else:
         return """
