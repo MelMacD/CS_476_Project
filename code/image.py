a082863b99149cd0c49497b2f9bba36f3c6ecabe
MelMacD <37010895+MelMacD@users.noreply.github.com>
2018-11-11 14:07:16 -0600

    Update image.py

--- code/image.py
+++ code/image.py
@@ -1,6 +1,7 @@
 class Image:
     def __init__(self, row):
         self.html = self.buildHtml()
+        #setId(row.get("id"));
         #setLocation(row.get("top"), row.get("left"))
         #setSize(row.get("width"), row.get("height"))
         #setDepth(row.get("depth"));
@@ -9,11 +10,15 @@ class Image:
     #@override
     def buildHtml(self):
         return """
-<div class="draggable resizableAspect image" style="width: {width}px; height: {height}px; position: absolute; z-index: {depth}; left: {left}px; top: {top}px;">
+<div id="{id}" class="draggable resizableAspect image" style="width: {width}px; height: {height}px; position: absolute; z-index: {depth}; left: {left}px; top: {top}px;">
     <button class="editImage edit" type="button" style="position: absolute; top: 0; right: 0;" data-toggle="modal" data-target="#exampleModal">Edit</button>
     <img src="{src}" style="width: 100%; height: 100%;">
 </div>"""
       
+    #@override
+    def setId(self, id):
+         return self.html.format(id=id)
+        
     #@override
     def setLocation(self, top, left):
          return self.html.format(top=top, left=left)
