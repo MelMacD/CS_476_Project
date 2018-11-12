46f958680ca0ac54d6aef4528c9b09178ebf10fb
MelMacD <37010895+MelMacD@users.noreply.github.com>
2018-11-12 01:50:35 -0600

    Update post.py

--- code/post.py
+++ code/post.py
@@ -1,51 +1,61 @@
 class Post:
     def __init__(self, row):
-        self.html = self.buildHtml()
-        setId(row[1]);
-        #setLocation(row[2], row[3])
-        #setSize(row[4], row[5])
-        #setDepth(row[6]);
-        #setTitle(row[7]);
-        #setBody(row[8]);
-        #setBackground(row[9]);
-        #setFontColor(row[10]);
+        self.id = setId(row);
+        self.top = setTop(row)
+        self.left = setLeft(row)#inherit base attributes with super
+        self.width = setWidth(row)
+        self.height = setHeight(row)
+        self.depth = setDepth(row[6]);
+        self.title = setTitle(row[7]);
+        self.body = setBody(row[8]);
+        self.background = setBackground(row[9]);
+        self.fontColor = setFontColor(row[10]);
 
     #@override
     def buildHtml(self):
         return """
-<div id="{id}" class="border border-dark rounded draggable resizable post" style="width: {{width}}px; height: {{height}}px; position: absolute; z-index: {{depth}}; left: {{left}}px; top: {{top}}px;">
+<div id="{id}" class="border border-dark rounded draggable resizable post" style="width: {width}px; height: {height}px; position: absolute; z-index: {depth}; left: {left}px; top: {top}px;">
     <button class="editPost edit" type="button" style="position: absolute; top: 0; right: 0;" data-toggle="modal" data-target="#exampleModal">Edit</button>
-    <div id="originalContent" style="width: 100%; height: 100%; background-color: {{backgroundColor}}; color: {{fontColor}};">
-        <h3>{{title}}</h3>
-        <p>{{content}}</p>
+    <div id="originalContent" style="width: 100%; height: 100%; background-color: {backgroundColor}; color: {fontColor};">
+        <h3>{title}</h3>
+        <p>{content}</p>
     </div>
-</div>"""
+</div>""".format(id=self.id, top=self.top, left=self.left, width=self.width, height=self.height, depth=self.depth,
+                title=self.title, content=self.body, backgroundColor=self.background, fontColor=self.fontColor)
       
     #@override
-    def setId(self, id):
-        self.html.format(id=id)
+    def setId(self, row):
+        return row[1]
         
     #@override
-    def setLocation(self, top, left):
-        self.html.format(top=top, left=left)
+    def setTop(self, row):
+        return row[2]
      
     #@override
-    def setSize(self, width, height):
-        self.html.format(width=width, height=height)
+    def setLeft(self, row):
+        return row[3]
+    
+    #@override
+    def setWidth(self, row):
+        return row[4]
      
     #@override
-    def setDepth(self, depth):
-        self.html.format(depth=depth)
+    def setHeight(self, row):
+        return row[5]
+    
+    #@override
+    def setDepth(self, row):
+        return row[6]
       
-    def setTitle(self, title):
-        self.html.format(title=title)
+    def setTitle(self, row):
+        return row[7]
       
-    def setBody(self, content):
-        self.html.format(content=content)
+    def setBody(self, row):
+        return row[8]
 
-    def setBackground(self, backgroundColor):
-        self.html.format(backgroundColor=backgroundColor)
+    def setBackground(self, row):
+        return row[9]
      
-    def setFontColor(self, fontColor):
-        self.html.format(fontColor=fontColor)
+    def setFontColor(self, row):
+        return row[10]
      
