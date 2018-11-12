812440c77982884fa3446e7c28195e61fa186c53
MelMacD <37010895+MelMacD@users.noreply.github.com>
2018-11-12 00:29:35 -0600

    Update database.py

--- code/database.py
+++ code/database.py
@@ -8,11 +8,13 @@ class Database:
         self.password = '$up3rSecret'
         self.driver = '{ODBC Driver 13 for SQL Server}'
         self.connection = self.connect()
+        self.rowCount = 0
       
     def connect(self):
         return pyodbc.connect('DRIVER='+self.driver+';SERVER='+self.server+';PORT=1433;DATABASE='+self.database+';UID='+self.username+';PWD='+self.password)
     
     def execute(self, isModifying, command):
+        self.rowCount = 0
         try:
             cursor = self.connection.cursor()
             cursor.execute(command)
@@ -20,6 +22,7 @@ class Database:
                 self.connection.commit()
                 return "Success"
             else:
+                self.rowCount = cursor.rowcount
                 return cursor.fetchall()
         except pyodbc.Error as e:
             sqlstate = ex.args[1]
@@ -28,3 +31,5 @@ class Database:
     def disconnect(self):
         self.connection.close()
         
+    def getRowCount(self):
+        return self.rowCount
