6f49d60b477a58617e97cc0a7528dbf3d36764d4
MelMacD <37010895+MelMacD@users.noreply.github.com>
2018-11-11 20:58:17 -0600

    Update database.py

--- code/database.py
+++ code/database.py
@@ -8,9 +8,10 @@ class Database:
         self.username = 'cs476'
         self.password = '$up3rSecret'
         self.driver = '{ODBC Driver 13 for SQL Server}'
+        self.connection = self.connect()
       
     def connect(self):
-        return pyodbc.connect('DRIVER='+driver+';SERVER='+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+password)
+        return pyodbc.connect('DRIVER='+self.driver+';SERVER='+self.server+';PORT=1433;DATABASE='+self.database+';UID='+self.username+';PWD='+self.password)
     
     def execute(self, isModifying, command):
         try:
