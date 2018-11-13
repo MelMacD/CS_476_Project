ad4ee010536e1c374fbe3de820d96c8e61debf44
Iyanu01 <37347177+Iyanu01@users.noreply.github.com>
2018-11-13 11:16:00 -0700

    Update bloglist.py

--- code/bloglist.py
+++ code/bloglist.py
@@ -78,22 +78,7 @@ html {
 </style>
   </head>
   <body>
-  <div class="row">
-  <div class="column">
-    <div class="card">
-    
-    <img src="/w3images/team1.jpg" alt="Jane" style="width:100%">
-      <div class="container">
-        <h2>Jane Doe</h2>
-        <p class="title">CEO & Founder</p>
-        <p>Some text that describes me lorem ipsum ipsum lorem.</p>
-        <p>example@example.com</p>
-    
-    
-    <p><button class="button">View</button></p>
-    </div>
-    </div>
-    </div>
+  
   </body>
   </html>
   """.format(blogContent=buildBlogContent())
