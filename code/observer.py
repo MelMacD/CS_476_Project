94ab14fc6132bedc5a33065c39c4c6f1d647c573
Iyanu01 <37347177+Iyanu01@users.noreply.github.com>
2018-11-13 08:06:36 -0700

    Update observer.py

--- code/observer.py
+++ code/observer.py
@@ -1,4 +1,35 @@
 import abc
+class Subject:
+
+    def __init__(self):
+        self._observers = set()
+        self._subject_state = None
+
+    def attach(self, observer):
+        observer._subject = self
+        self._observers.add(observer)
+
+    def detach(self, observer):
+        observer._subject = None
+        self._observers.remove(observer)
+
+    def _notify(self):
+        for observer in self._observers:
+            observer.update(self._subject_state)
+            
+class SQL(Subject):
+      def __init__(self, name=''): 
+            Subject.__init__(self)
+            self.name = name
+            self.sql = 0
+            
+      
+     def getData(self):
+        return self.sql
+
+
+
+
 
 class Observer(metaclass=abc.ABCMeta):
 
