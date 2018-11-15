12b38faa48beb75ef73f8a6602459ac4bcc179ff
Iyanu01 <37347177+Iyanu01@users.noreply.github.com>
2018-11-15 13:14:50 -0700

    Update bloglist.py

--- code/bloglist.py
+++ code/bloglist.py
@@ -9,6 +9,11 @@ from code.database import Database as database
 
 def bloglist():
     if request.method == 'POST':
+        db = database()
+         #querying the database and checking ""
+        queryBuilder = query("blog")
+        queryString = queryBuilder.selectAllFilter("blogName='test'")
+        db.execute(True, queryString)
              return ""
     else:
             return "error"
