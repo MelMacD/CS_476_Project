4dac89ad76edcc1019ac8ae2dfe9604d4cd1a4b8
MelMacD <37010895+MelMacD@users.noreply.github.com>
2018-11-15 14:36:53 -0600

    Create react.py
    build basic react class

---
+++ code/react.py
@@ -0,0 +1,33 @@
+class React:
+    def __init__(self, values):
+        self.reactions = self.buildReactions(values)
+    
+    #@override
+    def buildHtml(self):
+        return """
+        <div class="reactBar" style="height: 38px; background-color: lightgray; position: absolute; width: 100%; bottom: 0px;">
+            <div class="dropdown">
+                <button class="btn btn-secondary dropdown-toggle react" style="position: absolute; top: 0; right: 0; z-index: 1;" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">React</button>
+                <div class="dropdown-menu">
+                    <div style="margin-left: 13px;">
+                        <button class="reactLike" type="button"><i class="em em---1"></i></button>
+                        <button class="reactDislike" type="button"><i class="em em--1"></i></button>
+                        <button class="reactClap" type="button"><i class="em em-clap"></i></button>
+                    </div>
+                    <div style="margin-left: 13px;">
+                        <button class="reactHeart" type="button"><i class="em em-heart"></i></button>
+                        <button class="reactSmile" type="button"><i class="em em-smile"></i></button>
+                        <button class="reactCry" type="button"><i class="em em-sob"></i></button>
+                    </div>
+                    <div style="margin-left: 13px;">
+                        <button class="reactSilly" type="button"><i class="em em-stuck_out_tongue_winking_eye"></i></button>
+                        <button class="reactAngry" type="button"><i class="em em-angry"></i></button>
+                        <button class="reactShock" type="button"><i class="em em-scream"></i></button>
+                    </div>
+                </div>
+            </div>
+        </div>
+"""
+
+    def buildReactions(self, values):
+        return ""
