3c848c3d7f30f102ad83565aa87c9e8fbc96844f
MelMacD <37010895+MelMacD@users.noreply.github.com>
2018-11-17 13:06:57 -0600

    Update react.py
    fix styling for reactions

--- code/react.py
+++ code/react.py
@@ -7,14 +7,30 @@ class React:
         return """
         <div class="reactBar" style="height: 38px; background-color: lightgray; position: absolute; width: 100%; bottom: 0px;">
             <div class="reactions" style="position: absolute; left: 0px; width: 65%; font-size: 12px;">
-                <span>0<i class="em em---1 smallEmoji"></i></span>
-                0<i class="em em--1 smallEmoji"></i>
-                0<i class="em em-clap smallEmoji"></i>
-                0<i class="em em-heart smallEmoji"></i>
-                0<i class="em em-smile smallEmoji"></i>
-                0<i class="em em-sob smallEmoji"></i>
-                0<i class="em em-stuck_out_tongue_winking_eye smallEmoji"></i>
-                <i class="em em-angry smallEmoji"></i>
+                <i class="em em---1 smallEmoji" style="position: absolute; margin-left: 14px;">
+                    <p style="position: absolute; left: -14px;">0</p>
+                </i>
+                <i class="em em--1 smallEmoji" style="position: absolute; margin-left: 14px;">
+                    <p style="position: absolute; left: -14px;">0</p>
+                </i>
+                <i class="em em-clap smallEmoji" style="position: absolute; margin-left: 14px;">
+                    <p style="position: absolute; left: -14px;">0</p>
+                </i>
+                <i class="em em-heart smallEmoji" style="position: absolute; margin-left: 14px;">
+                    <p style="position: absolute; left: -14px;">0</p>
+                </i>
+                <i class="em em-smile smallEmoji" style="position: absolute; margin-left: 14px;">
+                    <p style="position: absolute; left: -14px;">0</p>
+                </i>
+                <i class="em em-sob smallEmoji" style="position: absolute; margin-left: 14px;">
+                    <p style="position: absolute; left: -14px;">0</p>
+               </i>
+                <i class="em em-stuck_out_tongue_winking_eye smallEmoji" style="position: absolute; margin-left: 14px;">
+                    <p style="position: absolute; left: -14px;">0</p>
+                </i>
+                <i class="em em-angry smallEmoji" style="position: absolute; margin-left: 14px;">
+                    <p style="position: absolute; left: -14px;">0</p>
+                </i>
                 <i class="em em-scream smallEmoji" style="position: absolute; margin-left: 14px;">
                     <p style="position: absolute; left: -14px;">0</p>
                 </i>
