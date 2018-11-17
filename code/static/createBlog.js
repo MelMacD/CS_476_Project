4b5c9e3588a3d87b41c53196f05adb682747b64b
MelMacD <37010895+MelMacD@users.noreply.github.com>
2018-11-17 17:16:25 -0600

    Update createBlog.js
    implement image upload and relevant functionality

--- code/static/createBlog.js
+++ code/static/createBlog.js
@@ -1,1 +1,33 @@
-
+// TODO: Enforce validation on input fields
+$(document).ready(function() {
+    $("#useUrl").on("change", function () {
+        $("#imageUrl").css( "display", "block" );
+        $("#imageBlobSelector").prop( "display", "none" );
+        $("#imageFile").prop( "display", "none" );
+        $("#submitImageUpload").prop( "display", "none" );
+    });
+    $("#useLibrary").on("change", function () {
+        $("#imageUrl").prop( "display", "none" );
+        $("#imageBlobSelector").prop( "display", "block" );
+        $("#imageFile").prop( "display", "block" );
+        $("#submitImageUpload").prop( "display", "block" );
+    });
+  
+    $("#imageUrl").on("change", function() {
+        imageUrl = $(this).val();
+        $("#imagePreview").attr("src", imageUrl);
+    });
+  
+    $.getJSON("/getBlobImages", function(data) {
+        let i;
+        $("#imageBlobSelector").empty();
+        $("#imageBlobSelector").append( "<option value='none'>None</option>" );
+        for (i = 0; i < data.length; i++) {
+            $("#imageBlobSelector").append( "<option value='" + data[i] + "'>" + data[i] + "</option>" );
+        }
+    });
+    $("#imageBlobSelector").on("change", function() {
+        libraryUrl = 'https://expressiveblob.blob.core.windows.net/images/' + $(this).val()
+        $("#imagePreview").attr("src", libraryUrl);
+    });
+});
