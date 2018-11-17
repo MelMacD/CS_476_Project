eda12627b1a60d0acdb1ba07304a2c8ec65313c1
MelMacD <37010895+MelMacD@users.noreply.github.com>
2018-11-17 17:23:03 -0600

    Update createBlog.py
    include create blog js

--- code/createBlog.py
+++ code/createBlog.py
@@ -22,6 +22,7 @@ class CreateBlog:
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<script src="static/createBlog.js"></script>
 <style>
 body {font-family: "Lato", sans-serif}
 .mySlides {display: none}
