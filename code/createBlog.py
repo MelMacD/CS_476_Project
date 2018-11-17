ccf2c5813179cacf54edd610e872038ce6f48ec9
MelMacD <37010895+MelMacD@users.noreply.github.com>
2018-11-17 17:27:04 -0600

    Update createBlog.py

--- code/createBlog.py
+++ code/createBlog.py
@@ -23,6 +23,12 @@ class CreateBlog:
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <script src="static/createBlog.js"></script>
+<script
+  src="https://code.jquery.com/jquery-3.3.1.min.js"
+  integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+  crossorigin="anonymous"></script>
+<script src="static/jquery-ui.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 <style>
 body {font-family: "Lato", sans-serif}
 .mySlides {display: none}
