c0ab4deb24d06d90291a242f6f84f1e8353511ce
Iyanu01 <37347177+Iyanu01@users.noreply.github.com>
2018-11-17 12:10:03 -0700

    Update statistics.py

--- code/statistics.py
+++ code/statistics.py
@@ -1,4 +1,13 @@
-<!-- Google Analytics -->
+from code import app
+from flask import request
+from code.database import Database as database
+
+
+
+
+
+
+
 <script>
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
