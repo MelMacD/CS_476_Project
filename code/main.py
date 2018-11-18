145cc306279863529712ba455c7e3a86b87ec201
MelMacD <37010895+MelMacD@users.noreply.github.com>
2018-11-18 14:06:28 -0600

    Update main.py

--- code/main.py
+++ code/main.py
@@ -28,18 +28,9 @@ body {font-family: "Lato", sans-serif}
   <div class="w3-bar w3-black w3-card">
     <a class="w3-bar-item w3-button w3-padding-large w3-hide-medium w3-hide-large w3-right" href="javascript:void(0)" onclick="myFunction()" title="Toggle Navigation Menu"><i class="fa fa-bars"></i></a>
     <a href="/" class="w3-bar-item w3-button w3-padding-large">Home</a>
-    <a href="login" class="w3-bar-item w3-button w3-padding-large w3-hide-small">Login</a>
-    <a href="signup" class="w3-bar-item w3-button w3-padding-large w3-hide-small">Sign-up</a>
-    
-    </div>
+    <a href="login" class="w3-bar-item w3-button w3-padding-large w3-hide-small login">Login</a>
+    <a href="signup" class="w3-bar-item w3-button w3-padding-large w3-hide-small signup">Sign-up</a>
   </div>
-</div>
-
-<!-- Navbar on small screens (remove the onclick attribute if you want the navbar to always show on top of the content when clicking on the links) -->
-<div id="navDemo" class="w3-bar-block w3-black w3-hide w3-hide-large w3-hide-medium w3-top" style="margin-top:46px">
-  <a href="login" class="w3-bar-item w3-button w3-padding-large" onclick="myFunction()">Login</a>
-  <a href="signup" class="w3-bar-item w3-button w3-padding-large" onclick="myFunction()">Sign-up</a>
-
 </div>
 
 <!-- Page content -->
