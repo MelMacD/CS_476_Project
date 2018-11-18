1f9295b3cca2752b291a625235d24ea41ca710de
MelMacD <37010895+MelMacD@users.noreply.github.com>
2018-11-18 14:18:35 -0600

    Update logInOut.js

--- code/static/logInOut.js
+++ code/static/logInOut.js
@@ -3,6 +3,8 @@ $(document).ready(function() {
     // if logged in, hide login and signup in the navbar
     if (loginToken.indexOf("userId") != -1) {
         alert("Logged in");
+        let values = loginToken.split("=");
+        alert(values[1]);
     }
     // read cookie
     // if hit logout, remove cookie
