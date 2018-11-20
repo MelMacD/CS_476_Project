3466c978fdfdaf27ab20d3ea41a0547390fd8181
MelMacD <37010895+MelMacD@users.noreply.github.com>
2018-11-19 23:17:20 -0600

    Update statistics.js

--- code/static/statistics.js
+++ code/static/statistics.js
@@ -4,8 +4,9 @@ $(document).ready(function() {
     // need to figure out rankings and update the right table rows with their ranking
     // would need to count the number of commenters and reactions for each and compare them
     // then need to compare values and assign rankings
+    // store as json, then iterate through and set ranking and display accordingly
     let values = {};
     $("tr").each( function( ) {
-        alert($(this).find("td")[3].text());
+        alert($(this).find(".commenters").text());
     });
 });
