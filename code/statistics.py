367cadaa19123c6dc4c58f81cf6158901f2665a8
MelMacD <37010895+MelMacD@users.noreply.github.com>
2018-11-19 22:16:05 -0600

    Update statistics.py
    include js file

--- code/statistics.py
+++ code/statistics.py
@@ -28,7 +28,7 @@ class Statistics:
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/dt/dt-1.10.18/datatables.min.css"/>
 <script type="text/javascript" src="https://cdn.datatables.net/v/dt/dt-1.10.18/datatables.min.js"></script>
-
+<script src="static/statistics.js"></script>
 </head>
 <body>
     <!-- Navbar -->
