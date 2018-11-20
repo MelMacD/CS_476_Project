d837c7b741e5dbc50510eeb70612a17c6421a6f0
MelMacD <37010895+MelMacD@users.noreply.github.com>
2018-11-20 12:04:58 -0600

    Update logInOut.js

--- code/static/logInOut.js
+++ code/static/logInOut.js
@@ -2,9 +2,10 @@ $(document).ready(function() {
     let loginToken = document.cookie;
     // if logged in, hide login and signup in the navbar
     if (loginToken.indexOf("userId") != -1) {
-        alert("Logged in");
         let values = loginToken.split("=");
-        alert(values[1]);
+        //values[1] is the user id
+        $(".login").css("display", "none");
+        $(".signup").css("display", "none");
     }
     // read cookie
     // if hit logout, remove cookie
