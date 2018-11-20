f337e65d6f63c5e8f90feb77e51be45a4131613d
MelMacD <37010895+MelMacD@users.noreply.github.com>
2018-11-20 12:13:32 -0600

    Update logInOut.js

--- code/static/logInOut.js
+++ code/static/logInOut.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
     let loginToken = document.cookie;
-    // if logged in, hide login and signup in the navbar
     if (loginToken.indexOf("userId") != -1) {
         let values = loginToken.split("=");
         //values[1] is the user id
@@ -15,8 +14,6 @@ $(document).ready(function() {
     }
     
     $(".logout").on("click", function() {
-        alert("logout");
+        document.cookie = "userId=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
     });
-    // read cookie
-    // if hit logout, remove cookie
 });
