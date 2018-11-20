8106615a333f7abbd03f71b15a34f4af626939df
MelMacD <37010895+MelMacD@users.noreply.github.com>
2018-11-19 20:19:42 -0600

    Update bloglist.py
    rehash bloglist page

--- code/bloglist.py
+++ code/bloglist.py
@@ -1,103 +1,64 @@
-
 from code import app
 from flask import request, render_template
 from code.sql_query_builder import SQLQueryBuilder as query
 from code.database import Database as database
 
-html = """
-<!DOCTYPE html>
-<html>
+class BlogList:
+    def __init__(self):
+        self.html = self.buildContent()
+      
+    #abstract, inherited
+    def buildContent(self):
+        return """
 <head>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<title> Blog List</title>
-<style>
-html {
-  box-sizing: border-box;
-}
-*, *:before, *:after {
-  box-sizing: inherit;
-}
-.column {
-  float: left;
-  width: 33.3%;
-  margin-bottom: 16px;
-  padding: 0 8px;
-}
-@media screen and (max-width: 650px) {
-  .column {
-    width: 100%;
-    display: block;
-  }
-}
-.card {
-  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
-}
-.container {
-  padding: 0 16px;
-}
-.container::after, .row::after {
-  content: "";
-  clear: both;
-  display: table;
-}
-.title {
-  color: grey;
-}
-.button {
-  border: none;
-  outline: 0;
-  display: inline-block;
-  padding: 8px;
-  color: white;
-  background-color: #000;
-  text-align: center;
-  cursor: pointer;
-  width: 100%;
-}
-.button:hover {
-  background-color: #555;
-}
-</style>
-  </head>
-  <body>
-  <h2>The Blog-List Section</h2>
-  <div class="row">
-  <div class="column">
-    <div class="card">
-    <img src=>
-    <div class ="container">
-  <h2> </h2> 
-  
- <tr>
-  <td>Username</td>
-  <td>Blog-Name</td>
-  <td>Image</td>
-  <td>Description</td>
-  </tr>
-    
-    {% for item in blog %}
-<tr>
-    <td>{{item[0]}}</td>
-    <td>{{item[1]}}</td>
-    ...
-</tr>
-{% endfor %}
-    </div>
-    </div>
+<title>Test Page</title>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.2/css/bootstrap-theme.min.css">
+<link href="https://afeld.github.io/emoji-css/emoji.css" rel="stylesheet">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<script
+  src="https://code.jquery.com/jquery-3.3.1.min.js"
+  integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+  crossorigin="anonymous"></script>
+<script src="static/jquery-ui.min.js"></script>
+<link rel="stylesheet" type="text/css" href="//ajax.googleapis.com/ajax/libs/jqueryui/1.7.1/themes/base/jquery-ui.css"/>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</head>
+<body>
+    <!-- Navbar -->
+    <div class="w3-top" style="z-index: 1000;">
+        <div class="w3-bar w3-black w3-card">
+            <a class="w3-bar-item w3-button w3-padding-large w3-hide-medium w3-hide-large w3-right" href="javascript:void(0)" onclick="myFunction()" title="Toggle Navigation Menu"><i class="fa fa-bars"></i></a>
+            <a href="/" class="w3-bar-item w3-button w3-padding-large">Home</a>
+            <a href="login" class="w3-bar-item w3-button w3-padding-large w3-hide-small">Login</a>
+            <a href="signup" class="w3-bar-item w3-button w3-padding-large w3-hide-small">Sign-up</a>
         </div>
-  </body>
-</html>
-"""
+    </div>
+    <div id="blogList">
+    </div>
+</div>
+</body>"""
+    
+    def displayBlogs(self):
+        return
+    
+    def createElement(self):
+        return
 
     
 @app.route("/bloglist", methods=['GET', 'POST'])
 
 def bloglist():
-    if request.method == 'GET':
-        requestData = request.get_json()
-        db = database()
-        queryBuilder = query("blog")
-        queryString = queryBuilder.selectAllFilter("blogName='test'")
-        result = []
-        return html
+    blogList = BlogList()
+    return blogList.html
+    #if request.method == 'GET':
+    #    requestData = request.get_json()
+    #    db = database()
+    #    queryBuilder = query("blog")
+    #    queryString = queryBuilder.selectAllFilter("blogName='test'")
+    #    result = []
     
