8259b97fcac7c6601c0410ee463d7c45f9ccfdd9
MelMacD <37010895+MelMacD@users.noreply.github.com>
2018-11-19 23:13:54 -0600

    Update statistics.js

--- code/static/statistics.js
+++ code/static/statistics.js
@@ -1,3 +1,11 @@
 $(document).ready(function() {
     $("#statsTable").DataTable();
+    
+    // need to figure out rankings and update the right table rows with their ranking
+    // would need to count the number of commenters and reactions for each and compare them
+    // then need to compare values and assign rankings
+    let values = {};
+    $("tr").each( function( ) {
+        alert($(this).find("td")[3].text());
+    });
 });
