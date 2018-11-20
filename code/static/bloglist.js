b155293c2304f6e924fb142082174895e7696498
MelMacD <37010895+MelMacD@users.noreply.github.com>
2018-11-19 21:45:13 -0600

    Update bloglist.js

--- code/static/bloglist.js
+++ code/static/bloglist.js
@@ -1,16 +1,8 @@
 $(document).ready(function() {
     //Tasks:
-    // 1. Populate empty sources with the default image
-    $("img").each( function() {
-        if ($(this).attr("src") === "") {
-            $(this).attr("src", "/static/default.js");
-        }
-    });
     // 2. Make tab clickable
     $("li").on("click", function() {
-        alert("clicked list item");
-        alert($(this).find("h1").text());
-        //window.location = "https://expressyourself.azurewebsites.net/bloglist?blogName=test";
+        window.location = "https://expressyourself.azurewebsites.net/blogView?blogName=" + $(this).find("h1").text();
     });
     // 3. Implement searching
 });
