11baa492fc6e87e00e1459e97ac3e0c71e49c491
MelMacD <37010895+MelMacD@users.noreply.github.com>
2018-11-21 00:16:00 -0600

    Update blogView.py

--- code/blogView.py
+++ code/blogView.py
@@ -238,7 +238,7 @@ def hello():
             <a href="signup" class="w3-bar-item w3-button w3-padding-large" onclick="myFunction()">Sign-up</a>
         </div>
     </div>
-    <div id="blogBody" style="font-family: {font};">
+    <div id="blogBody" style="font-family: {font}; height: 1500px;">
       {blogContent}
     </div>
     
