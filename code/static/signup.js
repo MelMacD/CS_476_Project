100ebf4eb46e74a5b8866c2b2e5cc06a86535b69
MelMacD <37010895+MelMacD@users.noreply.github.com>
2018-11-20 21:06:43 -0600

    Update signup.js

--- code/static/signup.js
+++ code/static/signup.js
@@ -1,14 +1,14 @@
-let errorPresent = false;
+let errorPresent;
 
 $(document).ready(function() {
     $("#SignUp").on("submit", function(e) {
-        e.preventDefault();
+        errorPresent = false;
         usernameValidation();
         emailValidation();
         pwdValidation();
         pwdReentryValidation();
         if ( errorPresent == true ) {
-            //e.preventDefault()
+            e.preventDefault()
         }
     });
 });
@@ -16,39 +16,39 @@ $(document).ready(function() {
 function usernameValidation() {
     if ( $("#username").val().length > 15 ) {
         $("#username_msg").text("Username must not be more than 15 characters.");
+        errorPresent = true;
     }
     else {
         $("#username_msg").text("");
     }
-    errorPresent = true;
 }
 
 function emailValidation() {
     if ( $("#email").val().length > 30 ) {
         $("#email_msg").text("Email must not be more than 30 characters.");
+        errorPresent = true;
     }
     else {
         $("#email_msg").text("");
     }
-    errorPresent = true;
 }
 
 function pwdValidation() {
     if ( $("#password").val().length > 15 ) {
         $("#pswd_msg").text("Password must not be more than 15 characters.");
+        errorPresent = true;
     }
     else {
         $("#pswd_msg").text("");
     }
-    errorPresent = true;
 }
 
 function pwdReentryValidation() {
     if ( $("#rePassword").val() !== $("#password").val() ) {
         $("#reEnter_msg").text("Password fields must match.");
+        errorPresent = true;
     }
     else {
         $("#reEnter_msg").text("");
     }
-    errorPresent = true;
 }
