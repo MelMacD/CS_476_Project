bce51872e926e3b007d8a4896b54edfbd0ec58ae
MelMacD <37010895+MelMacD@users.noreply.github.com>
2018-11-21 13:13:20 -0600

    Update blobStorage.py
    implement subject

--- code/blobStorage.py
+++ code/blobStorage.py
@@ -4,12 +4,14 @@ import json
 from flask import request
 from werkzeug.utils import secure_filename
 from azure.storage.blob import BlockBlobService, PublicAccess
+from code.subject import Subject
 
-class BlobStorage:
+class BlobStorage(Subject):
     def __init__(self):
         self.accountName = "expressiveblob"
         self.accountKey = "F2G8lu/eZ6PduDIJFksWvuItZdhf+GONR2wgwgSsJMUO4s0mMdFI6PiC7K7ypcMSOH6m5kPhn2C9ketBRQiyKA=="
-
+        super().__init__()
+        
     def connect(self):
         return BlockBlobService(account_name=self.accountName, account_key=self.accountKey)
 
@@ -17,7 +19,7 @@ class BlobStorage:
 
 def getBlobImages():
     blobStorage = BlobStorage()
-    block_blob_service = blobStorage.connect()
+    block_blob_service = blobStorage.connection
     generator = block_blob_service.list_blobs('images')
     blobList = []
     for blob in generator:
@@ -28,7 +30,7 @@ def getBlobImages():
 
 def getBlobVideos():
     blobStorage = BlobStorage()
-    block_blob_service = blobStorage.connect()
+    block_blob_service = blobStorage.connection
     generator = block_blob_service.list_blobs('videos')
     blobList = []
     for blob in generator:
@@ -44,7 +46,7 @@ def uploadBlobImage():
             if file:
                 filename = secure_filename(file.filename)
                 blobStorage = BlobStorage()
-                block_blob_service = blobStorage.connect()
+                block_blob_service = blobStorage.connection
                 container = 'images'
                 block_blob_service.create_blob_from_stream(container, filename, file)
                 return "Success"
@@ -64,7 +66,7 @@ def uploadBlobVideo():
             if file:
                 filename = secure_filename(file.filename)
                 blobStorage = BlobStorage()
-                block_blob_service = blobStorage.connect()
+                block_blob_service = blobStorage.connection
                 container = 'videos'
                 block_blob_service.create_blob_from_stream(container, filename, file)
                 return "Success"
