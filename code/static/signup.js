6d0438df1f42674434bb330cc307f61029ecb2e3
MelMacD <37010895+MelMacD@users.noreply.github.com>
2018-11-20 20:48:40 -0600

    Update signup.js

--- code/static/signup.js
+++ code/static/signup.js
@@ -4,6 +4,7 @@ $(document).ready(function() {
         e.preventDefault();
         let errorPresent = false;
         usernameValidation();
+        
         if ( errorPresent == true ) {
             //e.preventDefault()
         }
@@ -11,7 +12,7 @@ $(document).ready(function() {
 });
 
 function usernameValidation() {
-    if ( $("#username").val() == "" || $("#username").val() > 15 ) {
+    if ( $("#username").val() == "" || $("#username").val().length > 15 ) {
         $("username_msg").text("Username must not be empty and not more than 15 characters.");
     }
     else {
