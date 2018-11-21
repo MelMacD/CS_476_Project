b4dbc0489ecce00eea0f68af8a520756b20bf5dc
MelMacD <37010895+MelMacD@users.noreply.github.com>
2018-11-20 20:42:20 -0600

    Update signup.js
    username validation

--- code/static/signup.js
+++ code/static/signup.js
@@ -1,1 +1,20 @@
 
+$(document).ready(function() {
+    $("#SignUp").on("submit", function(e) {
+        e.preventDefault();
+        let errorPresent = false;
+        usernameValidation();
+        if ( errorPresent == true ) {
+            //e.preventDefault()
+        }
+    });
+});
+
+function usernameValidation() {
+    if ( $("#username").val() == "" || $("#username").val() > 15 ) {
+        $("username_msg").text("Username must not be empty and not more than 15 characters.");
+    }
+    else {
+        $("username_msg").text("");
+    }
+}
