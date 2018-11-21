ec881500996b7587fa542a95706b520433daf786
MelMacD <37010895+MelMacD@users.noreply.github.com>
2018-11-20 19:07:13 -0600

    Update blogView.py

--- code/blogView.py
+++ code/blogView.py
@@ -22,7 +22,6 @@ def uploadComment():
         queryBuilder = query("comments")
         queryString = queryBuilder.insertRow("'{blogName}', '{attachedToId}', '{currentUser}', '{comment}'".format(blogName=blogUrlName,
                 attachedToId=requestData.get("attachedToId"), currentUser=request.cookies.get('userId'), comment=requestData.get("comment")))
-        return queryString
         db.execute(True, queryString)
         db.disconnect()
         return str(requestData)
