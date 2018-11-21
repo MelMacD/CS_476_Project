07ae6f036eae7e7c8643e7491008bfc9bb1fcf4f
MelMacD <37010895+MelMacD@users.noreply.github.com>
2018-11-20 20:51:24 -0600

    Update signup.js

--- code/static/signup.js
+++ code/static/signup.js
@@ -12,10 +12,20 @@ $(document).ready(function() {
 });
 
 function usernameValidation() {
-    if ( $("#username").val() == "" || $("#username").val().length > 15 ) {
+    alert ($("#username").val().length);
+    if ( $("#username").val().length > 15 ) {
         $("username_msg").text("Username must not be empty and not more than 15 characters.");
     }
     else {
         $("username_msg").text("");
+    }
+}
+
+function emailValidation() {
+    if ( $("#email").val().length > 30 ) {
+        $("email_msg").text("Email must not be empty and not more than 30 characters.");
+    }
+    else {
+        $("email_msg").text("");
     }
 }
